f10f4eb35841003169d849f5cb682d6944ed75fe
Nashrul Khairil <nashrul@gmail.com>
2025-11-16 12:53:53 +0800

    Fix UI layout + globals + tailwind v4

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import UsageForm from '../components/UsageForm'
+import UsageForm from "../components/UsageForm";
 
 export default function Home() {
   return (
-    <main className="p-6">
-      <UsageForm />
+    <main className="min-h-screen flex justify-center items-start py-10 px-4 bg-[#e9eefb]">
+      <div className="w-full max-w-3xl">
+        <UsageForm />
+      </div>
     </main>
-  )
+  );
 }
